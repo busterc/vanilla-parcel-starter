f09adf5e15ff1dddfd962719f655c036f58921fe
Buster Collings <busterc@gmail.com>
2019-07-09 16:58:02 -0500

    Update with for Cordova
    Now accounts for being on a device!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-'use strict';
+var app = {
+  initialize: function() {
+    document.addEventListener(
+      'deviceready',
+      this.onDeviceReady.bind(this),
+      false
+    );
+  },
+
+  onDeviceReady: function() {
+    navigator.notification.alert('device is ready', () => {}, 'sup');
+  }
+};
+
+app.initialize();
